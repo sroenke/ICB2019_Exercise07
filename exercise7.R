17dc178f625b0ad70704e1149524f1a30a8c65f2
sroenke <sroenke@nd.edu>
2019-11-13 11:24:50 -0600

    adding part 2

--- exercise7.R
+++ exercise7.R
@@ -1,5 +1,6 @@
-#part1
-
+#1 puts even rows into a dataframe called 'oddrows'
+#useage:getoddrows(dataframe)
+irisdata<-read.csv("iris.csv")
 getoddrows<-function(x){
   rows<-nrow(x)
   columns<-ncol(x)
@@ -8,6 +9,33 @@ getoddrows<-function(x){
     if (i%%2!=0){
       oddrows[((i+1)/2),]<-x[i,]
     }
-    }
-  return(oddrows)
+  }
+  
 }
+
+
+#2 Part 1, returns number of observations of an inputted species
+#usage:speciescount(speciesName)
+speciescount <- function(species){
+  observations<-subset(irisdata,irisdata$Species==species)
+  assign<-nrows(observations)
+  print(assign)
+  
+}
+
+
+
+#2 Part 2, makes a dataframe called 'set' that contains the flowers with sepal width greated than the specified value
+#usage:Size(sepalWidthTarget)
+Size<-function(value){
+  set<<-subset(irisdata,irisdata$Sepal.Width>value)
+  
+}
+#2 part 3, makes a csv file called the species name containing only observations from that species
+#usage:filespecies(SpeciesName)
+filespecies<- function(species){
+  speciesgroup<-subset(irisdata,irisdata$Species==species)
+  write.csv(speciesgroup,file=paste(species))
+  
+}
+
